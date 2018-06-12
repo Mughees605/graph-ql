ea5e26ab11c7b87221e1bab1188ad6dfd07da335
mughees605 <mughees605@gmail.com>
2018-06-12 18:19:08 +0500

    Connecting to mLab lec 16 # 1.0

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
-
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://mughees:mughees605@ds153980.mlab.com:53980/graphql')
+mongoose.connection.once('open', () => {
+  console.log('connected to database')
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
@@ -11,5 +16,5 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+  console.log('now listening for requests on port 4000');
 });
