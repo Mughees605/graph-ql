4eefb4ae8e7776a10fe9e3b225eead9300a13ae5
mughees605 <mughees605@gmail.com>
2018-06-13 02:31:23 +0500

    addBook Mutation lec 19 # 1.0

--- schema/schema.js
+++ schema/schema.js
@@ -86,9 +86,26 @@ const Mutation = new GraphQLObjectType({
                     age: args.age
                 });
 
-              return  author.save();
+                return author.save();
             }
 
+        },
+
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                })
+                return book.save();
+            }
         }
     }
 })
