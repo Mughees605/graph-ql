58222474a34c18e76251477a48f5132825fe8746
mughees605 <mughees605@gmail.com>
2018-06-14 03:44:58 +0500

    intial commit for graphqlwithsql

---
+++ graphql-with-sql/schema/schema.js
@@ -0,0 +1,82 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const { Client } = require('pg');
+
+const connectionString = 'postgres://dbuser:dbUser321@auroradb.cu311zl9hdtl.us-east-1.rds.amazonaws.com:5432/test'
+const client = new Client({
+    connectionString,
+})
+
+client.connect((err) => {
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('connected')
+    }
+})
+
+const {
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: () => ({
+        user_id: { type: GraphQLID },
+        firstname: { type: GraphQLString },
+        lastname: { type: GraphQLString },
+        email: { type: GraphQLString },
+        workspace: {
+            type: WorkspaceType,
+            resolve(parent, args) {
+                const queryText = `SELECT * FROM public."Workspace" WHERE user_id = '${parent.user_id}'`;
+                return client.query(queryText).then((d) => {
+                    return d.rows[0]
+                })
+            }
+        }
+    })
+})
+
+const WorkspaceType = new GraphQLObjectType({
+    name: 'WorkspaceType',
+    fields: () => ({
+        user_id: { type: GraphQLID },
+        name: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                const userQuery = `SELECT * FROM users WHERE user_id = '${args.id}'`
+                return client.query(userQuery).then((d) => {
+                    return d.rows[0]
+                })
+            }
+        },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parent, args) {
+                const queryText = `SELECT * FROM users`;
+                return client.query(queryText).then((d) => {
+                    return d.rows
+                })
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
