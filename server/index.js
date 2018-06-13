4b8c029231bc66e1e511c00f5ebb8d9f5eb79ab4
mughees605 <mughees605@gmail.com>
2018-06-13 12:57:39 +0500

    Making Queries from React lec 26 # 1.0

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
+
+// cross origin request
+app.use(cors());
 
 mongoose.connect('mongodb://mughees:mughees605@ds153980.mlab.com:53980/graphql')
 mongoose.connection.once('open', () => {
